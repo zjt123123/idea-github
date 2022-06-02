6a13a7a797dd2b6930f2bf7b90ddf69df8aa501c
朱锦涛 <1315675171@qq.com>
2022-06-02 11:07:48 +0800

    第一次新建文件

---
+++ src/main/java/com/atguigu/git/test.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * Date:2022/6/2
+ * Author:zjc
+ * Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("新建文件");
+    }
+}
